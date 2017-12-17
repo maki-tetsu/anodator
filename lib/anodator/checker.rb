d32c6c9df0f77f0fd0c281145de8735831828321
Tetsuhisa MAKINO <tim.makino@gmail.com>
2017-12-17 16:32:44 +0900

    Fix Anodator::Checker

--- lib/anodator/checker.rb
+++ lib/anodator/checker.rb
@@ -4,31 +4,31 @@ require 'anodator/output_spec'
 require 'anodator/anodator_error'
 
 module Anodator
+  # Checker class.
   class Checker
-    def initialize(input_spec, rule_set, default_output_spec, configuration_check = false)
-      @input_spec   = input_spec
-      @rule_set     = rule_set
-      @output_specs = [default_output_spec]
+    def initialize(input_spec, rule_set, output_spec, config_check = false)
+      validate_initialize_params(input_spec, rule_set, output_spec)
 
-      unless @input_spec.is_a? InputSpec
-        raise ArgumentError, 'input_spec must be InputSpec object'
-      end
-      unless @rule_set.is_a? RuleSet
-        raise ArgumentError, 'rule_set must be RuleSet object'
-      end
-      unless @output_specs.first.is_a? OutputSpec
-        raise ArgumentError, 'default_output_spec must be OutputSpec object'
-      end
-
+      @input_spec = input_spec
+      @rule_set = rule_set
+      @output_specs = [output_spec]
       Validator::Base.values = @input_spec
 
-      validate_configuration if configuration_check
+      validate_configuration if config_check
+    end
+
+    def validate_initialize_params(input_spec, rule_set, output_spec)
+      [
+        [input_spec, InputSpec],
+        [rule_set, RuleSet],
+        [output_spec, OutputSpec]
+      ].each do |c|
+        raise ArgumentError, "#{c[0]} must be #{c[1]}" unless c[0].is_a? c[1]
+      end
     end
 
     def validate_configuration
-      # RuleSet
       @rule_set.validate_configuration
-      # OutputSpec
       @output_specs.each(&:validate_configuration)
     end
 
