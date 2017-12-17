57eb128a540c1441cccb405aa5d3e6e1a2a6e7ec
Tetsuhisa MAKINO <tim.makino@gmail.com>
2017-12-17 21:03:34 +0900

    fix exception class not used

--- lib/anodator/common.rb
+++ lib/anodator/common.rb
@@ -5,7 +5,7 @@ module Anodator
         v = value_and_class[0]
         c = value_and_class[1]
 
-        raise ArgumentError, "#{v} must be #{c}" unless v.is_a? c
+        raise exception_class, "#{v} must be #{c}" unless v.is_a? c
       end
     end
   end
